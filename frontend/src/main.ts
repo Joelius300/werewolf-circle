9c1c0b1900220d9793fabbfd8576a048fbf010a1
Joelius300 <bstlng321@gmail.com>
2021-03-21 16:18:16 +0100

    Finally rename the last files + comment
    The issue might have been VS code which didn't actually rename the files on disk when it's only a case-change.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue';
 import piniaInstance from '@/stores/piniaProvider';
 import App from './App.vue';
 import router from './router';
-import { initializeTokenStore } from './stores/tokenStore';
+// import { initializeTokenStore } from './stores/tokenStore';
 
-// initializeTokenStore(); <-- uncomment when rejoining is supported
+// initializeTokenStore(); <-- uncomment when rejoining is supported BUT NOT HERE. I think this should be done before routing because the code here could in theory be run on the server if we used SSR (right?).
 
 const app = createApp(App)
   .use(piniaInstance)
