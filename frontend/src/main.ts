6eee738b23003184632cca2293012b88723f3b9c
Joelius300 <bstlng321@gmail.com>
2021-05-19 14:07:07 +0200

    WIP

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,6 @@ import { createApp } from 'vue';
 import piniaInstance from '@/stores/piniaProvider';
 import App from './App.vue';
 import router from './router';
-// import { initializeTokenStore } from './stores/tokenStore';
-
-// initializeTokenStore(); <-- uncomment when rejoining is supported BUT NOT HERE.
-// I think this should be done before routing because the code here could in theory
-// be run on the server if we used SSR (right?).
 
 const app = createApp(App)
   .use(piniaInstance)
